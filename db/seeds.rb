74fb8791c40bde22ae5e510f5fbcc8eaf7bc5863
Craig Lamont <craig@techiteasy.org.uk>
2015-10-30 12:43:10 +0000

    working with no ingredients show

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 
 Category.delete_all
 Recipe.delete_all
+Ingredient.delete_all
 
 breakfast = Category.create!(name: 'Breakfast')
 starter = Category.create!(name: 'Starters')
@@ -16,12 +17,28 @@ lunch = Category.create!(name: 'Lunch')
 dinner = Category.create!(name: 'Dinner')
 dessert = Category.create!(name: 'Dessert')
 
-Recipe.create!(name: 'Chicken Soup', instructions: 'Put all ingredients in a pot. Boil it. Eat it.', category_id: starter.id)
-Recipe.create!(name: 'Porridge', instructions: 'Cook oats in milk or water, with salt or sugar.', category_id: breakfast.id)
-Recipe.create!(name: 'Sandwich', instructions: 'Chop up bread. Put stuff in. Eat', category_id: lunch.id)
+chicken_soup = Recipe.create!(name: 'Chicken Soup', instructions: 'Put all ingredients in a pot. Boil it. Eat it.', category_id: starter.id)
+porridge = Recipe.create!(name: 'Porridge', instructions: 'Cook oats in milk or water, with salt or sugar.', category_id: breakfast.id)
+sandwich = Recipe.create!(name: 'Sandwich', instructions: 'Chop up bread. Put stuff in. Eat', category_id: lunch.id)
 Recipe.create!(name: 'Pea Soup', instructions: 'Put peas in water. Boil. Eat', category_id: starter.id)
 Recipe.create!(name: 'Steak Bake', instructions: 'Go to Greggs. Buy one', category_id: lunch.id)
 Recipe.create!(name: 'Steak', instructions: 'Cook it as required', category_id: dinner.id)
+curry = Recipe.create!(name: 'Curry', instructions: 'Cook and eat with rice.', category_id: dinner.id)
 Recipe.create!(name: 'Steak Pie', instructions: 'Put steak in pastry. Cook. Eat', category_id: dinner.id)
 Recipe.create!(name: 'Angel Delight', instructions: 'Put in milk. Whisk it. Eat it.', category_id: dessert.id)
-Recipe.create!(name: 'Rice pudding', instructions: 'Open tin. Heat up. Eat.', category_id: starter.id)
+ricepudding = Recipe.create!(name: 'Rice pudding', instructions: 'Open tin. Heat up. Eat.', category_id: starter.id)
+
+chicken = Ingredient.create!(name: 'Chicken')
+rice = Ingredient.create!(name: 'Rice')
+water = Ingredient.create!(name: 'Water')
+milk = Ingredient.create!(name: 'Milk')
+ham = Ingredient.create!(name: 'Ham')
+
+chicken_soup.ingredients << chicken
+chicken_soup.ingredients << water
+porridge.ingredients << milk
+porridge.ingredients << water
+sandwich.ingredients << chicken
+sandwich.ingredients << ham
+curry.ingredients << rice
+ricepudding.ingredients << rice

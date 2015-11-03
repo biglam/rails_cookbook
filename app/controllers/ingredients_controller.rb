a85aff7b841cea68d35c00f23546796a1309a262
Craig Lamont <craig@techiteasy.org.uk>
2015-11-03 15:20:46 +0000

    authorisation prompts added

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,6 @@
 class IngredientsController < ApplicationController
   before_action :set_ingredient, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :create]
 
   # GET /ingredients
   # GET /ingredients.json
--- app/controllers/quantities_controller.rb
+++ app/controllers/quantities_controller.rb
@@ -1,5 +1,6 @@
 class QuantitiesController < ApplicationController
   before_action :set_quantity, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :create]
 
   # GET /quantities
   # GET /quantities.json
